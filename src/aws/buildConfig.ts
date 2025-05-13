31470a1ee1d8eb703026074b941f333d7f00f285
Bernardo Codesido <bernardo@iovlabs.org>
2025-05-13 13:56:02 -0300

    fix: fix safe_adressess from aws dynamodb

--- src/aws/buildConfig.ts
+++ src/aws/buildConfig.ts
@@ -1,19 +1,26 @@
 import { Schema } from "../config/schema.js";
+import logger from "../logger.js";
 import type { SecretStored } from "./index.js";
 import { DynamoDB, getSecrets } from "./index.js";
 
 export async function buildConfig(): Promise<Schema> {
   const secretStored: SecretStored = await getSecrets();
   const dynamoDB = new DynamoDB();
-  const addresses = (await dynamoDB.getItems(
+
+  const addresses = await fetchFormattedAddresses(
+    dynamoDB,
     secretStored.safeAddressesTable,
-  )) as { address: `${string}:0x${string}`; alias: string }[];
-  const signers = await dynamoDB.getItems(secretStored.safeSignersTable);
+  );
+  const signers = await fetchFormattedSigners(
+    dynamoDB,
+    secretStored.safeSignersTable,
+  );
+
   const schema: Schema = {
     slackBotToken: secretStored.slackBotToken,
     slackChannelId: secretStored.slackChannelId,
-    safeAddresses: formatAddress(addresses),
-    signers: formatSigners(signers),
+    safeAddresses: addresses,
+    signers,
     safeURL: "https://app.safe.global",
     pollInterval: 20,
     api: "fallback",
@@ -22,33 +29,53 @@ export async function buildConfig(): Promise<Schema> {
   return Schema.parse(schema);
 }
 
+async function fetchFormattedAddresses(
+  dynamoDB: DynamoDB,
+  tableName: string,
+): Promise<
+  [
+    Partial<Record<`${string}:0x${string}`, string>>,
+    ...Partial<Record<`${string}:0x${string}`, string>>[],
+  ]
+> {
+  const rawAddresses = (await dynamoDB.getItems(tableName)) as {
+    address: string;
+    alias: string;
+  }[];
+  logger.debug(rawAddresses);
+
+  return formatAddress(rawAddresses).map(item => ({
+    [item.address]: item.alias,
+  })) as [
+    Partial<Record<`${string}:0x${string}`, string>>,
+    ...Partial<Record<`${string}:0x${string}`, string>>[],
+  ];
+}
+
+async function fetchFormattedSigners(
+  dynamoDB: DynamoDB,
+  tableName: string,
+): Promise<{ [key: string]: string }> {
+  const rawSigners = await dynamoDB.getItems(tableName);
+  return formatSigners(rawSigners);
+}
+
 function formatAddress(
   addresses: { address: string; alias: string }[],
-): [
-  Partial<Record<`${string}:0x${string}`, string>>,
-  ...Partial<Record<`${string}:0x${string}`, string>>[],
-] {
-  return [
-    addresses.reduce(
-      (acc, item) => {
-        if (/^[^:]+:0x[a-fA-F0-9]{40}$/.test(item.address)) {
-          acc[item.address as `${string}:0x${string}`] = item.alias;
-        }
-        return acc;
-      },
-      Object.create(null) as Partial<Record<`${string}:0x${string}`, string>>,
-    ),
-  ];
+): { address: `${string}:0x${string}`; alias: string }[] {
+  return addresses
+    .filter(item => /^[^:]+:0x[a-fA-F0-9]{40}$/.test(item.address))
+    .map(item => ({
+      address: item.address as `${string}:0x${string}`,
+      alias: item.alias,
+    }));
 }
 
 function formatSigners(signers: { address: string; alias: string }[]): {
   [key: string]: string;
 } {
   return signers.reduce(
-    (
-      acc: { [key: string]: string },
-      item: { address: string; alias: string },
-    ) => {
+    (acc, item) => {
       acc[item.address] = item.alias;
       return acc;
     },
